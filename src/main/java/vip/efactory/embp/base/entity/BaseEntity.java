6ece68e320505bc2b3c7f68d007575494decf50b
dbdu <1797890817@qq.com>
2020-08-13 20:24:04 +0800

    使用Java8的日期类型

--- src/main/java/vip/efactory/embp/base/entity/BaseEntity.java
+++ src/main/java/vip/efactory/embp/base/entity/BaseEntity.java
@@ -6,7 +6,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 
 /**
@@ -15,19 +15,19 @@ import java.util.Date;
 @Setter
 @Getter
 @JsonInclude(value = JsonInclude.Include.NON_NULL)      //null值的属性JSON不输出
-public abstract class BaseEntity<T extends BaseAdvanceSearchEntity> extends BaseAdvanceSearchEntity<T> {
+public abstract class BaseEntity<T extends BaseSearchEntity> extends BaseSearchEntity<T> {
 
     /**
      * 创建时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(hidden = true)
-    private Date createTime;
+    private LocalDateTime createTime;
     /**
      * 更新时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(hidden = true)
-    private Date updateTime;
+    private LocalDateTime updateTime;
 
 }

1a979564505c0bc4588c1817de22b4839c40499c
dbdu <1797890817@qq.com>
2020-08-24 20:46:18 +0800

    优化:实体新增时也插入更新时间和更新人

--- src/main/java/vip/efactory/embp/base/fill/MbpMetaObjectHandler.java
+++ src/main/java/vip/efactory/embp/base/fill/MbpMetaObjectHandler.java
@@ -4,13 +4,13 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 /**
  * 实体属性自动填充功能
  * 详情参阅：https://mybatis.plus/guide/auto-fill-metainfo.html
+ *
  * @author dbdu
  */
 @Slf4j
@@ -20,17 +20,23 @@ public class MbpMetaObjectHandler implements MetaObjectHandler {
 
     /**
      * 实体属性自动填充功能，插入填充,自动填充创建时间及创建人
+     *
      * @param metaObject 元数据
      */
     @Override
     public void insertFill(MetaObject metaObject) {
         log.debug("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "creatorNum", String.class, auditorAware.getCurrentAuditor().get());
+        LocalDateTime now = LocalDateTime.now();
+        String currentUser = auditorAware.getCurrentAuditor().get();
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, now);
+        this.strictInsertFill(metaObject, "creatorNum", String.class, currentUser);
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, now);
+        this.strictInsertFill(metaObject, "updaterNum", String.class, currentUser);
     }
 
     /**
      * 实体属性自动填充功能，更新填充,自动填充更新时间及更新人
+     *
      * @param metaObject 元数据
      */
     @Override
